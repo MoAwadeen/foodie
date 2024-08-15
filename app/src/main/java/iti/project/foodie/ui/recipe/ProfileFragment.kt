9bdd6f81fd91f512e7ba70e02c96952b3e698491
MoAwadeen <modyyahia44@gmail.com>
2024-08-16 01:22:30 +0300

    initialize master branch

--- app/src/main/java/iti/project/foodie/ui/recipe/ProfileFragment.kt
+++ app/src/main/java/iti/project/foodie/ui/recipe/ProfileFragment.kt
@@ -11,13 +11,13 @@ class ProfileFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //sevrerbethbrtbt
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 

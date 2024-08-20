e629fbb0e3b1dbf19bf0b1ab4dc93c02f4001c38
MoAwadeen <modyyahia44@gmail.com>
2024-08-21 00:18:19 +0300

    Handling Retrofit

--- app/src/main/java/iti/project/foodie/data/source/remote/network/ApiServices.kt
+++ app/src/main/java/iti/project/foodie/data/source/remote/network/ApiServices.kt
@@ -1,4 +1,10 @@
-package iti.project.foodie.data.source.remote.network;
+package iti.project.foodie.data.source.remote.network
+
+import iti.project.foodie.data.source.remote.model.MealList
+import retrofit2.Call
+import retrofit2.http.GET
 
 public interface ApiServices {
+    @GET("random.php")
+    fun getRandomMeal(): Call<MealList>
 }
--- app/src/main/java/iti/project/foodie/ui/adapters/HorizontalHomeAdapter.kt
+++ app/src/main/java/iti/project/foodie/ui/adapters/HorizontalHomeAdapter.kt
@@ -1,5 +1,6 @@
 package iti.project.foodie.ui.adapters
 
+import iti.project.foodie.R
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -8,22 +9,21 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import iti.project.foodie.R
 
-class HorizontalHomeAdapter(private val context: Context , private val arrayList: ArrayList<String>) :
+class HorizontalHomeAdapter(private val context: Context, private val arrayList: ArrayList<String>) :
     RecyclerView.Adapter<HorizontalHomeAdapter.ViewHolder>() {
 
-    private var onItemClickListener : OnItemClickListener ?= null
+    private var onItemClickListener: OnItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.horizontal_item_view, parent, false)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val imageUrl = arrayList[position]
 
-        if (imageUrl.isNotEmpty()) {
+        if (!imageUrl.isNullOrEmpty()) {
             Glide.with(context)
                 .load(imageUrl)
                 .apply(RequestOptions()
@@ -45,14 +45,20 @@ class HorizontalHomeAdapter(private val context: Context , private val arrayList
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imageView: ImageView = itemView.findViewById(R.id.categoryView)
+        var imageView: ImageView = itemView.findViewById(R.id.list_item_image)
     }
 
-    fun setOnItemClickListener(listener : OnItemClickListener?) {
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
         this.onItemClickListener = listener
     }
 
+    fun updateData(map: List<String>) {
+        arrayList.clear()
+        arrayList.addAll(map)
+        notifyDataSetChanged()
+    }
+
     interface OnItemClickListener {
-        fun onClick(imageView : ImageView?, path : String?)
+        fun onClick(imageView: ImageView?, path: String?)
     }
 }
